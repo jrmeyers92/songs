7c41089a708e0a7400b9627fa79258ca5ffd983d
Jake Meyers <jakejake92@yahoo.com>
2020-10-20 16:13:59 -0600

    created reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+export const songReducer = () => {
+	return [
+		{ title: "no scrubs", duration: "4:05" },
+		{ title: "macarena", duration: "2:30" },
+		{ title: "all star", duration: "3:49" },
+		{ title: "phone home", duration: "4:43" },
+	];
+};
