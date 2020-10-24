3d6724227d510c9d94a26238a16b50e288c1b95e
Jake Meyers <jakejake92@yahoo.com>
2020-10-24 07:44:10 -0600

    SongList component created

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { selectSong } from "../actions";
+import SongList from "./SongList";
 
 const App = () => {
-	return <div>app</div>;
+	return (
+		<div>
+			<SongList />
+		</div>
+	);
 };
 
 export default App;
